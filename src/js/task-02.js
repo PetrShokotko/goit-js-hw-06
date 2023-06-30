48da589040388f16da7b653191e880d7816f7a1a
PetrShokotko <shokotkop@gmail.com>
2023-06-30 20:15:07 +0300

    Update 2 tasks

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientRef = document.querySelector("#ingredients");
+const itemsRef = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  return item;
+});
+ingredientRef.append(...itemsRef)
