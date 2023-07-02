799bcac75f3a0484847154f2e7fab48da2db64ef
PetrShokotko <shokotkop@gmail.com>
2023-07-02 15:32:49 +0300

    Update2

---
+++ js/task-09.js
@@ -0,0 +1,14 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const changeColorBtn = document.querySelector('button.change-color')
+const bodyColor = document.querySelector('body')
+const colorValue = document.querySelector('span.color')
+changeColorBtn.addEventListener('click', e => {
+  let color = getRandomHexColor();
+  bodyColor.style.backgroundColor = color;
+  colorValue.textContent = color;
+})
