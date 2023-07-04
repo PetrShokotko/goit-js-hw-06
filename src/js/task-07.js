0b393867252491938f1400245fa42e3db63f50fa
PetrShokotko <shokotkop@gmail.com>
2023-07-04 11:32:43 +0300

    Update 4

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,6 +1,21 @@
-const inputChangeSize = document.querySelector('input#font-size-control')
-const changeSize = document.querySelector('span#text')
+// const inputChangeSize = document.querySelector('input#font-size-control')
+// const changeSize = document.querySelector('span#text')
 
-inputChangeSize.addEventListener('change', event => {
-    changeSize.style.fontSize = `${event.target.value }px`
-})
+// inputChangeSize.addEventListener('change', event => {
+//     changeSize.style.fontSize = `${event.target.value }px`
+// })
+
+function changeFontSize() {
+    const inputChangeSize = document.querySelector('input#font-size-control');
+    const changeSize = document.querySelector('span#text');
+
+    const handleInputChange = (event) => {
+        changeSize.style.fontSize = `${event.target.value}px`;
+    };
+
+    inputChangeSize.addEventListener('input', handleInputChange);
+}
+
+// changeFontSize();
+
+
